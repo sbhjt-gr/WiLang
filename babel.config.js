90d2801a53a04b7f8e63bac2fe51658001d74a20
Subhajit Gorai <sg_outlp@outlook.com>
2025-12-10 19:12:46 +0530

    feat: voice call QR translation pairing impl.

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,22 @@
 module.exports = function(api) {
   api.cache(true);
+  const plugins = [
+    ['module:react-native-dotenv', {
+      moduleName: '@env',
+      path: '.env',
+      blocklist: null,
+      allowlist: null,
+      safe: false,
+      allowUndefined: true,
+      verbose: false,
+    }],
+    'react-native-reanimated/plugin',
+  ];
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push('transform-remove-console');
+  }
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      }],
-      'react-native-reanimated/plugin',
-    ]
+    plugins,
   };
 };
